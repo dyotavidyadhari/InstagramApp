70cfc8f1e8779623a94b1d71f344ed71e6402964
taniarachel <672018370@student.uksw.edu>
2021-04-22 15:57:51 +0700

    UI change

--- app/src/main/java/com/example/instagramkita/Adapter/FeedAdapter.kt
+++ app/src/main/java/com/example/instagramkita/Adapter/FeedAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.instagramkita.R
@@ -28,15 +30,21 @@ class FeedAdapter (context: Context,private val posts: List<post>): RecyclerView
         private val caption = view.user_caption
         private val uname = view.feed_uname
         private val imgprofile  = view.feed_profile
-       // private val tag = isi nya tag
+        private val txttag = view.txttag
+        private val tagged = view.tagged
+
         fun bindholder(context: Context,posted:post){
             Glide.with(context).load(posted.image).into(img)
             Glide.with(context).load(posted.userprofile).into(imgprofile)
             uname.text = posted.username
             caption.text = posted.caption
+
+            tagged.setOnClickListener{
+                txttag.text = posted.tag
+                txttag.isVisible = true
+            }
         }
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedAdapter.MyViewHolder {
         return MyViewHolder(LayoutInflater.from(konteks).inflate(R.layout.feed,parent,false))
     }
